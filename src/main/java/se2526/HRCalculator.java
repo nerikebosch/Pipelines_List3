b40954a3ae4c83f28bce3868d07caa0313f78261
nerikeb <nerike.b@gmail.com>
2025-11-09 20:38:32 +0100

    Implemented getWorkoutZone method and added their coverage tests

--- src/main/java/se2526/HRCalculator.java
+++ src/main/java/se2526/HRCalculator.java
@@ -12,6 +12,23 @@ public class HRCalculator {
     }
 
     public static String getWorkoutZone(int age, int bpm){
-        return "?";
+        int maxHR = calculateMaxHR(age);
+        double percentage = (double) bpm / maxHR;
+
+        if (percentage < 0.5) {
+            return "Below Zone";
+        } else if (percentage < 0.6) {
+            return "Zone 1: Warm-up/Maintenance";
+        } else if (percentage < 0.7) {
+            return "Zone 2: Fat Burn/Fitness training";
+        } else if (percentage < 0.8) {
+            return "Zone 3: Aerobic(Endurance training)";
+        } else if (percentage < 0.9) {
+            return "Zone 4: Anaerobic(Hardcore training)";
+        } else if (percentage <= 1.0) {
+            return "Zone 5: Maximum effort";
+        } else {
+            return "Above Max";
+        }
     }
 }
