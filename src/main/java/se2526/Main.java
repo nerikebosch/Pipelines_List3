cd45f72580fd834e1fcd794d271df16404dd8161
nerikeb <nerike.b@gmail.com>
2025-11-09 18:38:11 +0100

    Initial commit

---
+++ src/main/java/se2526/Main.java
@@ -0,0 +1,7 @@
+package se2526;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/HRCalculatorTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import se2526.HRCalculator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HRCalculatorTest {
+
+    @Test
+    public void testValidMaxHR(){
+        var result = HRCalculator.calculateMaxHR(25);
+        assertEquals(195, result);
+    }
+}
